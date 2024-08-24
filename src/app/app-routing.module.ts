6c5ee95390d3bffa4d2c43b60d63abd0bdfd33e6
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-08-24 13:37:03 +0530

    router link added to new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DummyComponent } from './dummy/dummy.component';
+import { DataApiComponent } from './data-api/data-api.component';
+import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'dummy', component: DummyComponent}
+  { path: 'dummy', component: DummyComponent},
+  { path: 'dataApi', component: DataApiComponent},
+  { path: 'about', component: AboutComponent}
 ];
 
 @NgModule({
