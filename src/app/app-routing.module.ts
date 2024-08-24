32ae0217a2b0ffe88cfb3ea381d21e8c71e7cf9f
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-08-24 12:39:31 +0530

    header component basics designed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DummyComponent } from './dummy/dummy.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'dummy', component: DummyComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
