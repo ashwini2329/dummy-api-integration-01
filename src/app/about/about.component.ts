884b12158cca3619c6b9d2f1035adfddf62518da
Ashwini Kumar <shahi.ashwini29@gmail.com>
2024-08-27 11:58:19 +0530

    template driven forms

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 import { DummyServiceService } from '../dummy-service.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-about',
@@ -7,11 +8,33 @@ import { DummyServiceService } from '../dummy-service.service';
   styleUrls: ['./about.component.scss'],
   providers: [DummyServiceService]
 })
-export class AboutComponent implements OnInit {
-  public about = 'hello'
-  constructor(private dummyService: DummyServiceService){}
+export class AboutComponent implements OnInit, AfterViewInit {
+  @ViewChild('form') form!: NgForm;
+
+  public about = 'hello';
+
+  constructor(private dummyService: DummyServiceService) {}
 
   ngOnInit(): void {
-      this.about = this.dummyService.getAbout();
+    this.about = this.dummyService.getAbout();
+  }
+
+  ngAfterViewInit(): void {
+    if (this.form) {
+      this.form.valueChanges?.subscribe((value) => {
+        console.log('Form value changed:', value);
+      });
+    }
+  }
+
+  onSubmit(formData: NgForm) {
+    if (formData.invalid) {
+      return;
+    }
+    const enteredEmail = formData.value.email;
+    const enteredPassword = formData.value.password;
+    console.log(enteredEmail, enteredPassword);
+
+    formData.reset();
   }
 }
